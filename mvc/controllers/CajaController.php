31c2630afda0472d99b348fe3e72b40534e142f2
Puesto 2 <Puesto 2@192.168.2.111>
2014-02-14 17:53:25 -0300

    caja al 70%

--- mvc/controllers/CajaController.php
+++ mvc/controllers/CajaController.php
@@ -6,7 +6,7 @@ namespace CEIT\mvc\controllers
     use \CEIT\mvc\models;
     use \CEIT\mvc\views;
     
-    final class CajaController extends core\AController implements core\ICrud
+    final class CajaController extends core\AController
     {
         public function __construct()
         {
@@ -32,79 +32,201 @@ namespace CEIT\mvc\controllers
             $this->_view->render($this->_template, $this->_dataCollection);
         }
         
-        public function create()
-        {
-            $this->_template = $this->_template = BASE_DIR . "/mvc/templates/caja/{$this->_action}.html";
-        }
-
-        public function delete($id)
-        {
-            $this->_template = $this->_template = BASE_DIR . "/mvc/templates/caja/{$this->_action}.html";
-        }
-
-        public function detail($id)
-        {
-            $this->_template = $this->_template = BASE_DIR . "/mvc/templates/caja/{$this->_action}.html";
-        }
-
         public function index()
         {
             $this->_template = $this->_template = BASE_DIR . "/mvc/templates/caja/{$this->_action}.html";
-            
+
             if(!empty($_POST))
             {
-                var_dump($_POST, $_COOKIE);
+                var_dump($_POST);
                 
-                $tmpArray = array();
+                $tmpArray = array(
+                    'Pedidos'   =>  array(),
+                    'Items'     =>  array(),
+                );
                 
+                // Si se apreto el boton de Agregar un item del sistema.
                 if(isset($_POST['btnAgregarPedido']))
                 {
-                    $cookie = filter_input(INPUT_COOKIE, 'PedidosAgregados');
+                    $cookie = filter_input(INPUT_COOKIE, 'Caja');
                     if(!empty($cookie))
                     {
-                        $tmpArray[] = unserialize($cookie);
+                        $tmpArray = unserialize($cookie);
                     }
                     
                     $pedidoItem = filter_input(INPUT_POST, 'btnAgregarPedido', FILTER_SANITIZE_NUMBER_INT);
                     
                     if(!empty($pedidoItem))
                     {
-                        if(array_search($pedidoItem, $tmpArray) === false)
+                        $flagExist = false;
+                        
+                        foreach($tmpArray as $item)
                         {
-                            $tmpArray[] = (int)$pedidoItem;
+                            if($item == $pedidoItem)
+                            {
+                                $flagExist = true;
+                                break;
+                            }
+                        }
+                        
+                        if(!$flagExist)
+                        {
+                            $tmpArray['Pedidos'][] = (int)$pedidoItem;
                         }
                     }
                     
-                    $_COOKIE['PedidosAgregados'] = serialize($tmpArray);
-                    setcookie('PedidosAgregados', serialize($pedidoItem), time() + 3600);
+                    $_COOKIE['Caja'] = serialize($tmpArray);
+                    setcookie('Caja', serialize($tmpArray), time() + 3600);
+                }
+                
+                if(isset($_POST['btnAgregarItem']))
+                {
+                    $cookie = filter_input(INPUT_COOKIE, 'Caja');
+                    if(!empty($cookie))
+                    {
+                        $tmpArray = unserialize($cookie);
+                    }
+                    
+                    $variosIdItem = filter_input(INPUT_POST, 'txtIdItem', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $variosDesc = filter_input(INPUT_POST, 'txtDescripcion', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $variosPU = filter_input(INPUT_POST, 'txtPrecioUnitario', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                    $variosCant = filter_input(INPUT_POST, 'txtCantidad', FILTER_SANITIZE_NUMBER_INT);
+                    $variosImp = filter_input(INPUT_POST, 'txtImporte', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                    
+                    if(!empty($variosIdItem) && !empty($variosDesc) && !empty($variosPU) && !empty($variosCant) && !empty($variosImp))
+                    {
+                        $flagExist = false;
+                        
+                        foreach($tmpArray['Items'] as $item)
+                        {
+                            if($item['IdItem'] == $variosIdItem)
+                            {
+                                $flagExist = true;
+                                break;
+                            }
+                        }
+                                                
+                        if(!$flagExist)
+                        {
+                            $tmpArray['Items'][] = array(
+                                'IdItem'    =>  (string)$variosIdItem,
+                                'Desc'      =>  (string)$variosDesc,
+                                'PreUnit'   =>  (float)$variosPU,
+                                'Cantidad'  =>  (int)$variosCant,
+                                'Importe'   =>  (float)$variosImp,
+                            );
+                        }
+                    }
+                    
+                    $_COOKIE['Caja'] = serialize($tmpArray);
+                    setcookie('Caja', serialize($tmpArray), time() + 3600);
+                }
+                
+                if(isset($_POST['btnQuitarPedido']))
+                {
+                    $cookie = filter_input(INPUT_COOKIE, 'Caja');
+                    if(!empty($cookie))
+                    {
+                        $tmpArray = unserialize($cookie);
+                    }
+                    
+                    $btnValor = filter_input(INPUT_POST, 'btnQuitarPedido', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $hidType = filter_input(INPUT_POST, 'hidType', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    
+                    if(!empty($btnValor) && !empty($hidType))
+                    {
+                        switch($hidType)
+                        {
+                            case 'Pedido':
+                                foreach($tmpArray['Pedidos'] as $index => $item)
+                                {
+                                    if($btnValor == $item)
+                                    {
+                                        unset($tmpArray['Pedidos'][$index]);
+                                    }
+                                }
+                                break;
+
+                            case 'Item':
+                                foreach($tmpArray['Items'] as $index => $item)
+                                {
+                                    if($btnValor == $item['IdItem'])
+                                    {
+                                        unset($tmpArray['Items'][$index]);
+                                    }
+                                }
+                                break;
+                            
+                            default:
+                                trigger_error("Alguien modifico el valor del campo input hidType!", E_USER_ERROR);
+                                break;
+                        }
+                    }
+                    
+                    $_COOKIE['Caja'] = serialize($tmpArray);
+                    setcookie('Caja', serialize($tmpArray), time() + 3600);
                 }
             }
             
             if(!empty($_COOKIE))
             {
-                $cookie = filter_input(INPUT_COOKIE, 'PedidosAgregados');
+                $cookie = filter_input(INPUT_COOKIE, 'Caja');
                 if(!empty($cookie))
                 {
                     $tmpArray = unserialize($cookie);
+                    var_dump($tmpArray);
                     
-                    if(!is_array($tmpArray))
+                    if(count($tmpArray['Pedidos']) >= 1)
                     {
-                        $tmpArray = array($tmpArray);
+                        foreach($tmpArray['Pedidos'] as $item)
+                        {
+                            $model = new models\PedidoModel();
+                            $model->_idPedido = $item;
+                            $this->result = $this->_model['Pedidos']->SelectItemCaja($model);
+                            if(count($this->result) == 1)
+                            {
+                                $filename = BASE_DIR . "/mvc/templates/caja/table_1_content.html";
+                                $this->table_1_content .= file_get_contents($filename);
+
+                                foreach($this->result[0] as $key => $value)
+                                {
+                                    $this->table_1_content = str_replace('{' . $key . '}', $value, $this->table_1_content);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        $filename = BASE_DIR . "/mvc/templates/caja/table_1_content.html";
+                        $this->table_1_content .= file_get_contents($filename);
+
+                        $default = array(
+                            'IdPedido'  =>  '',
+                            'Descripcion'   =>  '',
+                            'PrecioUnitario'    =>  '',
+                            'Cantidad'  =>  '',
+                            'Importe'   =>  '',
+                        );
+                        
+                        foreach($default as $key => $value)
+                        {
+                            $this->table_1_content = str_replace('{' . $key . '}', $value, $this->table_1_content);
+                        }
                     }
                     
-                    foreach($tmpArray as $item)
+                    if(count($tmpArray['Items']) >= 1)
                     {
-                        $model = new models\PedidoModel();
-                        $model->_idPedido = $item;
-                        $this->result = $this->_model['Pedidos']->SelectItemCaja($model);
-                        if(count($this->result) == 1)
+                        foreach($tmpArray['Items'] as $row)
                         {
-                            $filename = BASE_DIR . "/mvc/templates/caja/table_1_content.html";
+                            $filename = BASE_DIR . "/mvc/templates/caja/table_1_content_1.html";
                             $this->table_1_content .= file_get_contents($filename);
-
-                            foreach($this->result[0] as $key => $value)
+                            
+                            if(is_array($row))
                             {
-                                $this->table_1_content = str_replace('{' . $key . '}', $value, $this->table_1_content);
+                                foreach($row as $key => $value)
+                                {
+                                    $this->table_1_content = str_replace('{' . $key . '}', $value, $this->table_1_content);
+                                } 
                             }
                         }
                     }
@@ -129,12 +251,16 @@ namespace CEIT\mvc\controllers
                 }
             }
         }
-
-        public function update($id)
+        
+        public function index_confirm()
         {
             $this->_template = $this->_template = BASE_DIR . "/mvc/templates/caja/{$this->_action}.html";
+            
+            if(!empty($_POST))
+            {
+                var_dump($_POST);
+            }
         }
-
     }
 }
 
